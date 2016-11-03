a6005a600a746ba7fdf6a1be06b9f01f07e8ca0f
Chris <caschoener@gmail.com>
2016-11-03 18:42:15 -0400

    setup stuff :)

--- my_memory.c
+++ my_memory.c
@@ -12,40 +12,62 @@
 #include <stdlib.h>
 #include "memalloc.h"
 
-void setup( int malloc_type, int mem_size, void* start_of_memory ) {
-/**
- *  Fill your code here
- *
-**/
-}
+#define FIRST_FIT       0
+#define BEST_FIT        1
+#define WORST_FIT       2
+#define BUDDY_SYSTEM    3
 
+typedef struct Hole_Node_
+{
+    int size;
+    int location;
+    struct Hole_Node_* next;
+} Hole_Node;
+
+void setup(int, int, void*);
+void *my_malloc(int);
+void my_free(void*);
+int num_free_bytes();
+int num_holes();
+
+//global vars for memory space information
+int memory_size;
+int malloc_type;
+void* memory_start;
+
+//linked list of holes, initially one node for entire space
+Hole_Node* hole_list;
+
+void setup( int malloc_type_, int mem_size, void* start_of_memory ) {
+    malloc_type = malloc_type_;
+    memory_size = mem_size;
+    memory_start = start_of_memory;
+
+    Hole_Node* starter_node = malloc(sizeof(Hole_Node));
+    starter_node->size = memory_size;
+    starter_node->location = memory_start;
+    hole_list = starter_node;
+}
+// searches through holes based on type, adds header at start of hole, updates holes list, returns location+1
 void *my_malloc(int size) {
-/**
- *  Fill your code here
- *
-**/
-	return (void*)-1;
+    if (malloc_type = BEST_FIT){
+        Hole_Node* curr = hole_list;
+        while (hole_list->next!= NULL)
+        {
+            
+        }
+    }
+
+    return (void*)-1;
 }
 
 void my_free(void *ptr) {
-/**
- *  Fill your code here
- *
-**/
 }
 
 int num_free_bytes() {
-/**
- *  Fill your code here
- *
-**/
     return 0;
 }
 
 int num_holes() {
-/**
- *  Fill your code here
- *
-**/
     return 0;
 }
